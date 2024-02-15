713a2bfe5bb252b54f72f545f59acf07b88aaea8
Adopcalipt <79842781+Adopcalipt@users.noreply.github.com>
2024-02-15 12:48:09 +0000

    Update main.cpp

--- RandomStart_Asi/RandomStart++/main.cpp
+++ RandomStart_Asi/RandomStart++/main.cpp
@@ -2,7 +2,6 @@
 	THIS FILE IS A PART OF GTA V SCRIPT HOOK SDK
 				http://dev-c.com			
 			(C) Alexander Blade 2015
-		--Modified By Adoppocalipt 2023--
 */
 
 #include "..\inc\main.h"
@@ -15,7 +14,6 @@ BOOL APIENTRY DllMain(HMODULE hInstance, DWORD reason, LPVOID lpReserved)
 	{
 	case DLL_PROCESS_ATTACH:
 		scriptRegister(hInstance, ScriptMain);
-		scriptRegisterAdditionalThread(hInstance, ScriptSecond);
 		keyboardHandlerRegister(OnKeyboardMessage);
 		break;
 	case DLL_PROCESS_DETACH:
